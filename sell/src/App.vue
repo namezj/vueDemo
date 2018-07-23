f688e35fa255fe1f57aad586bafcdcee2277dab8
1195978227 <454zjsmd>
2018-07-23 16:59:33 +0800

    添加

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header></header>
+    <div class="tab">
+      i am tab!
+    </div>
+    <div class="content">
+      i am content!
+    </div>
   </div>
 </template>
 
 <script>
+  import header from './components/header/header'
 export default {
-  name: 'App'
+  name: 'App',
+    components:{
+        header
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ sell/src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header">
+        i am header!
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: 'header'
+    }
+</script>
+
+<style scoped lang="stylus" type="stylesheet/stylus">
+
+</style>
