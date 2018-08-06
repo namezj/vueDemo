cdef18cc028694f50b7d827c126bd3f0a69566f4
1195978227 <454zjsmd>
2018-08-06 17:59:58 +0800

    添加商品列表详情页

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -15,7 +15,7 @@
                 <li v-for="item in goods" class="food-list food-list-hook">
                     <h1 class="title">{{item.name}}</h1>
                     <ul>
-                        <li v-for="food in item.foods" class="food-item border-1px">
+                        <li @click="selectFood(food,$event)" v-for="food in item.foods" class="food-item border-1px">
                             <div class="icon">
                                 <img width="57" height="57" :src="food.icon" alt="">
                             </div>
@@ -40,6 +40,7 @@
         </div>
         <shopcart ref="shopCart" :select-foods="selectFoods" :delivery-price="seller.deliveryPrice"
                   :min-price="seller.minPrice"></shopcart>
+        <food :food="selectedFood" ref="food"></food>
     </div>
 </template>
 
@@ -47,6 +48,7 @@
   import BScroll from 'better-scroll';
   import shopcart from '../shopcart/shopcart'
   import cartcontrol from '../cartcontrol/cartcontrol'
+  import food from '../food/food'
 
   const ERR_OK = 0;
   export default {
@@ -60,12 +62,14 @@
       return {
         goods: [],
         listHeight: [],
-        scrollY: 0
+        scrollY: 0,
+        selectedFood: {}
       };
     },
     components: {
       shopcart,
-      cartcontrol
+      cartcontrol,
+      food
     },
     computed: {//计算属性
       currentIndex() {
@@ -142,6 +146,13 @@
         this.$nextTick(() => {
           this.$refs['shopCart'].drop(el);　//调用shopcart组件的drop()函数
         });
+      },
+      selectFood(food, event) {
+        if (!event._constructed) {
+          return
+        }
+        this.selectedFood = food;
+        this.$refs.food.show();//调用子组件show（）方法
       }
     },
   }
